bf719cdd75e6dfe3ac591623f56deb07ce7e7736
Ramadhan Iman <rama.ramadhaniman@gmail.com>
2023-03-09 20:58:59 +0700

    menambahkan keterangan disetiap masing-masing function

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let tanahSebelumnya;
 let selesai;
 let skor;
 
+// function untuk menentukan tanah mana yang akan memunculkan tikus secara random
 function randomTanah(tanah) {
     const t = Math.floor(Math.random() * tanah.length);
     const tRandom = tanah[t];
@@ -18,10 +19,12 @@ function randomTanah(tanah) {
     return tRandom;
 }
 
+// function untuk membuat durasi min dan max munculnya tikus secara acak
 function randomWaktu(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
+// function untuk memunculkan tikus, durasi munculnya tikus & menghilangkan tikus
 function munculkanTikus() {
     const tRandom = randomTanah(tanah);
     const wRandom = randomWaktu(800, 1300);
@@ -47,12 +50,14 @@ function mulai() {
     }, 8000);
 }
 
+// function untuk menghitung pukulan yang masuk
 function pukul() {
     skor++;
     papanSkor.textContent = skor;
     this.parentNode.classList.remove('muncul');
 }
 
+// function untuk merubah kursor menjadi alat pukul tikus
 tikus.forEach(t => {
     t.addEventListener('click', pukul);
 });
